df61a97ced5e1c1dc38bf1f331da0d7a9b045b5f
Tom Williamson <tom@dtssoftware.com>
2016-04-28 12:22:52 -0400

    Initial Commit

---
+++ HighVoltage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighVoltage
+//
+//  Created by Tom Williamson on 4/28/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighVoltage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighVoltage
+//
+//  Created by Tom Williamson on 4/28/16.
+//  Copyright © 2016 Tom Williamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
